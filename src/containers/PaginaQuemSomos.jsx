d87768dc32efc1cde8b411b31b2aa8821029ba1b
Breno Pavanelli dos Santos <138505121+brenopavanelli@users.noreply.github.com>
2026-05-20 09:43:01 -0300

    feat: header e footer na PaginaQuemSomos

--- src/containers/PaginaQuemSomos.jsx
+++ src/containers/PaginaQuemSomos.jsx
@@ -1,7 +1,14 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
 export default function PaginaQuemSomos() {
   return (
-    <div className="min-h-screen bg-salgado-light">
+    <div>
+      <Header />
+      <div className="min-h-screen bg-salgado-light">
       <p>Quem Somos — em construção</p>
+      </div>
+      <Footer />
     </div>
   );
 }
